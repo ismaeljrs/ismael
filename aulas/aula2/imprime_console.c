81846a363fa2d0226caa80494f4d266b205eabdc
ismaeljrs <160501483+ismaeljrs@users.noreply.github.com>
2024-08-21 13:52:50 +0000

    codigos aula 2 pratica 2

---
+++ aulas/aula2/imprime_console.c
@@ -0,0 +1,32 @@
+//autor : Ismael jorge brandão
+//data: 21/08/2024
+//aula 03
+//
+#include <stdio.h>
+
+int main(){ 
+  
+  //imprima um numero inteiro 
+  printf("%i\n",10);
+  printf("%i %i\n", 11,22);
+  //imprima um numero decimal
+  printf("%f\n", 10.1234);
+  printf("%10.2f\n", 10.23453);
+  //imprima um caractere aspas simples 
+  printf("%c\n", 'A');
+  printf("%c\n", 69);
+  printf("%c\n", 65);
+
+//imprime um sprints
+  printf("%s\n", "bom dia !");
+  printf("%30s!\n","Quero cafe");
+  printf("%-30s!\n","Quero cafe");
+  
+  
+  
+  
+  
+  
+  
+  return 0;
+}
